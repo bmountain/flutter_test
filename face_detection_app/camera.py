c82f03a062563fb1fa4d84e4b6ea26da675c201f
tomo-aoyama <aoyama@stu.kobe-u.ac.jp>
2024-07-20 18:26:32 +0900

    added some comments

--- face_detection_app/camera.py
+++ face_detection_app/camera.py
@@ -1,30 +1,29 @@
 import cv2
 
-
+# 顔検出モデルの読み込み
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #faceDetect = cv2.dnn.readNetFromONNX('face_detection_yunet_2023mar.onnx')
 
+# カメラから画像を取得して顔を囲う枠を上書きするクラス
 class Video(object):
-    def __init__(self):
+    def __init__(self): 
+        # デフォルトのカメラを取得．
         self.video=cv2.VideoCapture(0)
     def __del__(self):
+        # カメラを閉じる
         self.video.release()
     def get_frame(self):
+        # カメラから画像データを取得．ret (boolean): 取得の成否，frame: 画像を表現するndarray (行数x列数x3色)
         ret,frame=self.video.read()
-        faces=faceDetect.detectMultiScale(frame, 1.3, 5)
+        # 検出された顔のリストを取得．各顔はx (左下のx座標)，y (左下のy座標), w (幅), h (高さ)で表現される
+        faces=faceDetect.detectMultiScale(frame, minSize = (50, 50))
+        # すべての顔の枠を付ける
         for x,y,w,h in faces:
-            x1,y1=x+w, y+h
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,255), 1)
-            cv2.line(frame, (x,y), (x+30, y),(255,0,255), 6) #Top Left
-            cv2.line(frame, (x,y), (x, y+30),(255,0,255), 6)
-
-            cv2.line(frame, (x1,y), (x1-30, y),(255,0,255), 6) #Top Right
-            cv2.line(frame, (x1,y), (x1, y+30),(255,0,255), 6)
-
-            cv2.line(frame, (x,y1), (x+30, y1),(255,0,255), 6) #Bottom Left
-            cv2.line(frame, (x,y1), (x, y1-30),(255,0,255), 6)
-
-            cv2.line(frame, (x1,y1), (x1-30, y1),(255,0,255), 6) #Bottom right
-            cv2.line(frame, (x1,y1), (x1, y1-30),(255,0,255), 6)
+            # 右上の頂点を計算
+            x1, y1 =x+w, y+h
+            # 枠を画像に書き込む．パラメタ：画像，二つの頂点，色，太さ
+            cv2.rectangle(frame, (x,y), (x1, y1), (192,192,192), 2)
+        # 画像をメモリバッファに書き込む
         ret,jpg=cv2.imencode('.jpg',frame)
+        # 画像(ndarray)をbyteに変換
         return jpg.tobytes()
